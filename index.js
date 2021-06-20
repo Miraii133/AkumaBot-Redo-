7bc439cb9e8c6d5fbebc2edc7a09442bae9e085f
Miraii133 <82250357+Miraii133@users.noreply.github.com>
2021-06-20 13:55:06 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('Bot online');
+});
+
+
+client.login('ODMwMzkyMzkyNTQ3ODI3NzIz.YHGBLg.ne2t9JV7NSAzMTsUzincD_KFHqc');
