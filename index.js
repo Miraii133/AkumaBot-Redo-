cb5cd8751e45ea53cf7e5762f6dee584ff5ce6c5
Miraii133 <82250357+Miraii133@users.noreply.github.com>
2021-06-22 11:18:16 +0800

    Working code.

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('./BotFiles/node_modules/discord.js');
+const bot = new Discord.Client();
+const fs = require('fs');
+const message = require('./BotFiles/events/message.js');
+const config = require('./config.json');
 
-client.on('ready', () => {
-  console.log('Bot online');
-});
+const eventFiles = fs.readdirSync('./BotFiles/events/').filter((file) =>
+  file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const event = require(`./BotFiles/events/${file}`);
+  if (event.once) {
+    bot.once(event.name, (...args) => event.execute(bot, ...args));
+  } else {
+    bot.on(event.name, (...args) => event.execute (...args));
+  }
+}
 
 
-client.login('ODMwMzkyMzkyNTQ3ODI3NzIz.YHGBLg.ne2t9JV7NSAzMTsUzincD_KFHqc');
+bot.login(config.token);
